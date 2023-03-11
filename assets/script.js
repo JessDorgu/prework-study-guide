2440235357f77e82894d7f1ac78fea7101a61220
unknown <jdorgu011@aol.com>
2023-03-11 03:23:58 -0500

    final changes

--- assets/script.js
+++ assets/script.js
@@ -25,4 +25,3 @@ console.log('Here are the topics we learned through Prework:');
 listTopics();
 console.log('Which topic should we study first?');
 selectTopic();
->>>>>>> ceebca1daa4c72f646e00828e642243a7a816088
